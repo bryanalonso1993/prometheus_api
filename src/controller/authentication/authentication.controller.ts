da52de3f47c423ee4df771e7186c547096d64578
bryanalonso1993 <bryalmeyda.7@gmail.com>
2022-10-02 22:40:32 -0500

    se modificaron los serivios de autenticacion

--- src/controller/authentication/authentication.controller.ts
+++ src/controller/authentication/authentication.controller.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { AuthenticationService } from "../../service";
 
+/**
+ * Esquemas de validacion de Token
+ */
+import { validateSchemaAuthentication, validateSchemaToken } from "../../middlewares/validateSchemaRequest";
 
 class AuthenticationController{
     private readonly router = Router();
@@ -10,17 +14,25 @@ class AuthenticationController{
         this.post();
     }
     get(){
+        // Validate Token
         this.router.get(
             '/validateToken',
+            (req:Request, res:Response, next:NextFunction) => validateSchemaToken(req, res, next),
             (req:Request, res:Response, next:NextFunction) => this.authenticationService.validateToken(req, res, next)
         );
+        // Respuesta para validar si el token es valido o no
         this.router.get(
             '/responseToken',
+            (req:Request, res:Response, next:NextFunction) => validateSchemaToken(req, res, next),
             (req:Request, res:Response) => this.authenticationService.responseToken(req, res)
         );
     }
     post(){
-        this.router.post('/createToken', (req:Request, res:Response) => this.authenticationService.createToken(req, res));
+        // Crear Token
+        this.router.post(
+            '/createToken',
+            (req:Request, res:Response) => this.authenticationService.createToken(req, res)
+        );
     }
     getRouter(){
         return this.router;
--- src/service/authentication/authentication.service.ts
+++ src/service/authentication/authentication.service.ts
@@ -9,7 +9,7 @@ class AuthenticationService {
      */
     createToken(req:Request, res:Response) {
         if ( !(req.path === '/createToken') ) return res.status(401).json({ error: 'Bad Route'});
-        if( !req.headers.authorization || req.headers.authorization.indexOf('Basic') === -1 ) return res.status(401).json({ error:'Missing Authorization Header' });
+        if( !req.headers.authorization || req.headers.authorization.indexOf('Basic') === -1 ) return res.status(401).json({ error:'Authentication Service, Missing Authorization Header' });
         const base64Credentials:string = req.headers.authorization.split(' ')[1];
         const credentials:string = Buffer.from(base64Credentials, 'base64').toString('ascii');
         const [ username, password ] = credentials.split(':');
@@ -17,7 +17,7 @@ class AuthenticationService {
         {
             const token = jwt.sign({
                 data:username
-            }, sanitzedConfig.SEED, { expiresIn: '360d'} );
+            }, sanitzedConfig.SEED, { expiresIn: '24h'} );
             res.status(200).json({ token });
         }else{
             return res.status(404).json({ error: 'Error de autenticacion '});
