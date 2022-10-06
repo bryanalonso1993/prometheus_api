32c0f5c7178fe7538d603e30360bd28a65336656
bryanalonso1993 <bryalmeyda.7@gmail.com>
2022-10-06 13:03:09 -0500

    se añadio funcionalidad pm

--- src/controller/pm/pm.controller.ts
+++ src/controller/pm/pm.controller.ts
@@ -1,14 +1,19 @@
-import { Request, Response, Router } from "express";
-import { PMService } from "../../service";
+import { NextFunction, Request, Response, Router } from "express";
+import { PMService, AuthenticationService } from "../../service";
 
 class PMController {
     private readonly router = Router();
     private readonly pmService = new PMService();
+    private readonly authenticationService = new AuthenticationService();
     constructor(){
         this.get();
     }
     get(){
-        this.router.get('/', (req:Request, res:Response) => this.pmService.getDevicesInventory(req, res));
+        this.router.get(
+            '/listdevicename',
+            (req:Request, res:Response, next:NextFunction) => this.authenticationService.validateToken(req, res, next),
+            (req:Request, res:Response) => this.pmService.getDevicesInventory(req, res)
+        );
     }
     getRouter(){
         return this.router;
