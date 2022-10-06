25e8907387b2e95fe1bc10e24b6e75de84972533
bryanalonso1993 <bryalmeyda.7@gmail.com>
2022-10-06 13:03:28 -0500

    se añadio funcionalidad prometheus

--- src/controller/prometheus/prometheus.controller.ts
+++ src/controller/prometheus/prometheus.controller.ts
@@ -1,18 +1,32 @@
-import { Router, Request, Response } from "express";
-import { PrometheusService } from "../../service";
+import { Router, Request, Response, NextFunction } from "express";
+import { validateMultipleRequestPrometheus } from "../../middlewares/validateSchemaRequest";
+import { PrometheusService, AuthenticationService } from "../../service";
 
 class PrometheusController{
     private readonly router = Router();
     private readonly prometheusService = new PrometheusService();
+    private readonly authenticationService = new AuthenticationService();
     constructor(){
         this.get();
-        // this.post();
     }
     get(){
         this.router.get('/', (req:Request, res:Response) => this.prometheusService.getDevices(req, res));
-    }
-    post(){
-        // this.router.post('/snmptxin', (req:Request, res:Response) => this.prometheusService.getTrafficOutSnmpDevices(req, res));
+        this.router.get(
+            '/device',
+            (req:Request, res:Response, next:NextFunction) => this.authenticationService.validateToken(req, res, next),
+            (req:Request, res:Response) => this.prometheusService.getDevices(req, res)
+        );
+        this.router.get(
+            '/interface',
+            (req:Request, res:Response, next:NextFunction) => this.authenticationService.validateToken(req, res, next),
+            (req:Request, res:Response) => this.prometheusService.getInterfaces(req, res)
+        );
+        this.router.get(
+            '/snmptx(inout)',
+            (req:Request, res:Response, next:NextFunction) => this.authenticationService.validateToken(req, res, next),
+            (req:Request, res:Response, next:NextFunction) => validateMultipleRequestPrometheus(req, res, next),
+            (req:Request, res:Response) => this.prometheusService.getTrafficSnmpDevices(req, res)
+            );
     }
     getRouter(){
         return this.router;
